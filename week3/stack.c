9b7e05b41f22fd8eb4bd3ee903b4863c91498dc0
Kanlayaphat <mybile.e70e@gmail.com>
2023-02-07 15:45:36 +0700

    new commit

---
+++ week3/stack.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct
+{
+    int data;
+    struct node *next;
+} node;
+
+void createList(node **ptr)
+{
+    if (*ptr == NULL)
+    {
+
+    }
+    else
+    {
+        
+    }
+}
+
+void display(node *ptr)
+{
+
+}
+
+void push(node **ptr) // insert new element
+{
+
+}
+
+void pop(node **ptr) // pop last element
+{
+
+}
+
+int main()
+{
+    node *ll;
+    createList(&ll);
+    return 0;
+}
